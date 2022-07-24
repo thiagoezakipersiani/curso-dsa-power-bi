c1d63d9059b65b0aa535aba0af7953b29c64a959
Thiago Ezaki Persiani <thiago.persiani@webbytelecom.local.br>
2022-07-24 20:24:48 -0300

    Update miniProjeto03.R

--- Cap15/miniProjeto03.R
+++ Cap15/miniProjeto03.R
@@ -112,3 +112,58 @@ missmap(dados_clientes, main = "Valores missing observados")
 dados_clientes <- na.omit(dados_clientes)
 missmap(dados_clientes, main="Valores missing observados")
 dim(dados_clientes)
+
+#Alterando a variavel dependente para o tipo fator
+str(dados_clientes$inadimplente)
+collnames(dados_clientes)
+dados_clientes$inadimplente <- as.factor(dados_clientes$inadimplente)
+View(dados_clientes)
+
+#Total de indimplentes versus não-inadimplentes
+?table
+table(dados_clientes$inadimplente)
+
+#Vejamos as porcentagens entre as classes
+prop.table(table(dados_clientes$inadimplente))
+
+#Plot da distribuição usando ggplot2
+qplot(inadimplente, data= dados_clientes, geom="bar") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#Set seed
+set.seed(12345)
+
+#Amostragem estratificada
+#Seleciona as linhas de acordo com a variável inadimplente como strata
+?createDataPartition
+indice <- createDataPartition(dados_clientes$inadimplente, p= 0.75, list=FALSE)
+dim(indice)
+
+#Definimos os dados de treinamento como subconjunto do conjunto de dados original
+#Com números de indice de linha (conforme indicado acima) E TODAS as colunas
+dados_treino <- dados_clientes[indice,]
+table(dados_treino$inadimplente)
+
+#Veja a porcentagens entre as classes 
+prop.table(table(dados_treino$inadimplente))
+
+#Comparamos as porcentagens entre as classes de treinamento e dados originais
+compara_dados <- cbind(prop.table(table(dados_treino$inadimplente)),
+                                  prop.table(table(dados_clientes$inadimplente)))
+colnames(compara_dados) <-c("Treinamento", "Original")
+compara_dados
+
+#Melt data - Converte colunas em linhas
+?reshape2::melt
+melt_compara_dados <- melt(compara_dados)
+melt_compara_dados
+
+#Plot para ver distribuição do treinamento vs original
+ggplot(melt_compara_dados, aes(x=X1, y=value)) +
+   geom_bar(aes(fill=X2), stat = "identity" , position = "dodge") +
+  theme(axis.text.x =  element_text(angle=90, hjust = 1))
+
+#Tudo o que não está no dataset de treinamento está no dataset de teste. Observe o sinal de - (menos)
+dados_teste <-dados_clientes[-indice,]
+dim(dados_teste)
+dim(dados_treino)
