56918456a26ea41a07e551a32aaab40c07eea42a
Thiago Ezaki Persiani <thiago.persiani@webbytelecom.local.br>
2022-08-16 12:43:41 -0300

    Cap 15

--- Cap15/miniProjeto03.R
+++ Cap15/miniProjeto03.R
@@ -260,35 +260,67 @@ ggplot(rankImportance,
 
 
 colnames(dados_treino_bal)
-modelo_v3 <-  randomForest(inadimplente ~ PAY_0 + PAY_2 + PAY_AMT2 + PAY_5 + BILL_AMT1,
-                           data= dados_treino_bal)
+modelo_v3 <- randomForest(inadimplente ~ PAY_0 + PAY_2 + PAY_3 + PAY_AMT1 + PAY_AMT2 + PAY_5 + BILL_AMT1, 
+                          data = dados_treino_bal)
 modelo_v3
 
-#Avaliando o modelo
+# Avaliando o modelo
 plot(modelo_v3)
 
-#Previsões com dados testes
+# Previsoes com dados de teste
 previsoes_v3 <- predict(modelo_v3, dados_teste)
 
-#Configurações matriz 
-cm_v3 <- caret::confusionMatrix(previsoes_v3,dados_teste$inadimplente,positive="1")
+# Confusion Matrix
+?caret::confusionMatrix
+cm_v3 <- caret::confusionMatrix(previsoes_v3, dados_teste$inadimplente, positive = "1")
 cm_v3
 
-#Calculando prevision, recall e f1 score, metricas de avaliação do modelo preditivo
+# Calculando Precision, Recall e F1-Score, metricas de avaliaçao do modelo preditivo
 y <- dados_teste$inadimplente
-v_pred_v3<-previsoes_v3
+y_pred_v3 <- previsoes_v3
 
-precision <- posPredValue(v_pred_v3,y)
+precision <- posPredValue(y_pred_v3, y)
 precision
 
-recall<- sensitivity(v_pred_v3,y)
+recall <- sensitivity(y_pred_v3, y)
 recall
 
-f1 <- (2* precision* recall) / (precision+recall)
-f1
+F1 <- (2 * precision * recall) / (precision + recall)
+F1
 
 #Salvando o modelo
 saveRDS(modelo_v3, file="modelo/modelo_v3.rds")
 
 #Carregando o modelo 
 modelo_final <- readRDS('modelo/modelo_v3.rds')
+
+# Dados dos clientes
+PAY_0 <- c(0, 0, 0) 
+PAY_2 <- c(0, 0, 0) 
+PAY_3 <- c(1, 0, 0) 
+PAY_AMT1 <- c(1100, 1000, 1200) 
+PAY_AMT2 <- c(1500, 1300, 1150) 
+PAY_5 <- c(0, 0, 0) 
+BILL_AMT1 <- c(350, 420, 280) 
+
+# Concatena em um dataframe
+novos_clientes <- data.frame(PAY_0, PAY_2, PAY_3, PAY_AMT1, PAY_AMT2, PAY_5, BILL_AMT1)
+View(novos_clientes)
+
+# Previsoes
+previsoes_novos_clientes <- predict(modelo_final, novos_clientes)
+
+# Checando os tipos de dados
+str(dados_treino_bal)
+str(novos_clientes)
+
+# Convertendo os tipos de dados
+novos_clientes$PAY_0 <- factor(novos_clientes$PAY_0, levels = levels(dados_treino_bal$PAY_0))
+novos_clientes$PAY_2 <- factor(novos_clientes$PAY_2, levels = levels(dados_treino_bal$PAY_2))
+novos_clientes$PAY_3 <- factor(novos_clientes$PAY_3, levels = levels(dados_treino_bal$PAY_3))
+novos_clientes$PAY_5 <- factor(novos_clientes$PAY_5, levels = levels(dados_treino_bal$PAY_5))
+str(novos_clientes)
+
+# Previsoes
+previsoes_novos_clientes <- predict(modelo_final, novos_clientes)
+View(previsoes_novos_clientes)
